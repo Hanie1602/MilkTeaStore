291fda7c18972554ea0f967d62d2cb104d6ef2e2
anhquan121203 <quannguyen121203@gmail.com>
2025-07-08 11:03:49 +0700

    fix code

--- app/src/main/java/com/example/milkteastore/controller/MainActivity.java
+++ app/src/main/java/com/example/milkteastore/controller/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.milkteastore.controller;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,8 +17,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        setContentView(R.layout.activity_main); // đảm bảo file XML này trùng tên layout XML
+
+        View rootView = findViewById(R.id.rootLayout); // đổi từ R.id.main -> R.id.rootLayout
+        ViewCompat.setOnApplyWindowInsetsListener(rootView, (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
